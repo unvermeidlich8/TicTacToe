cf2dc844541eebe11b008f5179497102cd0ac28e
Roman <romkapevchiy@gmail.com>
2023-10-15 22:24:41 +0300

    test_create_empty_board

---
+++ main.py
@@ -0,0 +1,7 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [' '] * 9
+
+
+
+game = TicTacToe
---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from main import TicTacToe
+
+
+class TicTacToeTest(unittest.TestCase):
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
